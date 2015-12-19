cc2a50f8746494075ddf187e18a0ec3ce153a85d
Ilya Dronov <dronoffilya@yandex.ru>
2015-12-19 14:05:42 +0300

    server api address

--- src/client/Constants.java
+++ src/client/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final String STUB_LOGIN = "dronov";
     public static final String STUB_PASSWORD = "12345";
 
-    public static final String SERVER_IP = "192.168.43.164";
+    public static final String SERVER_IP = "192.168.43.67";
     public static final int SERVER_TCP_PORT = 4445;
     public static final int BROADCAST_PORT = 5050;
 
